ccc477dc80fad137085ed818174cfd761841fe6e
louaibouzaher <jm_bouzaher@esi.dz>
2022-01-14 04:29:38 +0200

    Added production baseurl

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV == "production"
-    ? "https://moodme-challenge-server.herokuapp.com/"
+    ? "https://moodme-challenge-server.herokuapp.com"
     : "http://localhost:4321";
